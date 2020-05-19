d494cc442b07e19bb0491d7a5b857a1ab1fa4aaf
kickscar <kickscar@gmail.com>
2020-05-19 19:00:07 +0900

    init commit

---
+++ src/prob04/StringUtil.java
@@ -0,0 +1,6 @@
+package prob04;
+
+public class StringUtil {
+
+	//문자열을 결합하여 리턴 하는 메소드 구현    
+}
---
+++ src/prob06/Add.java
@@ -0,0 +1,12 @@
+package prob06;
+
+public class Add {
+	private int a;
+	private int b;
+	public void setValue(int a, int b) {
+
+	}
+	
+	public int calculate() {
+	}
+}
